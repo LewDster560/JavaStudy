95f20aad622da822b2b9e7c3b5a7ada46cebfdc1
liudong <ldqzhyx@163.com>
2019-07-31 18:08:32 +0800

    动态代理的用法

--- design/src/main/java/com/ld/design/dynamicProxy/GuiTai.java
+++ design/src/main/java/com/ld/design/dynamicProxy/GuiTai.java
@@ -1,5 +1,7 @@
 package com.ld.design.dynamicProxy;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
@@ -7,7 +9,10 @@ import java.lang.reflect.Method;
  * @author LiuDong
  * @date 2019-7-31
  */
+@Slf4j
 public class GuiTai implements InvocationHandler {
+
+    public String daiLiName = "代理人刘冬";
 
     private Object pinpai;
 
@@ -17,11 +22,12 @@ public class GuiTai implements InvocationHandler {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        System.out.println("================================代理begin");
-        System.out.println("柜台是" + this.getClass().getSimpleName());
-        System.out.println("品牌是" +  this.pinpai.getClass().getField("name").get(pinpai));
+        System.out.println();
+        log.info("================================代理begin");
+        System.out.println("柜台是:" + this.getClass().getField("daiLiName").get(this));
+        System.out.println("品牌是:" + this.pinpai.getClass().getField("name").get(pinpai));
         method.invoke(pinpai, args);
-        System.out.println("================================代理over");
+        log.info("================================代理over");
         return null;
     }
 }
